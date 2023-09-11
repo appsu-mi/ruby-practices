8519532654e96d8c963d0ccf13bb6cfa0301965b
appsu-mi <natubate870@gmail.com>
2023-09-11 19:07:47 +0900

    Calendar Refactoring

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -3,51 +3,36 @@
 require 'date'
 require 'optparse'
 
-@today = Date.today
 
-def calendar_head(year,month)
+def calendar_head(year, month)
   week_a_words = ['日', '月', '火', '水', '木', '金', '土']
-  first_day = Date.new(year, month, 1)
-  puts "      #{month}月 #{year}"
-  week_a_words.each {|word| print word.center(2)}
+  puts "#{month}月 #{year}".rjust(13)
+  week_a_words.each {|word| print "#{word}".ljust(2)}
   puts "\n"
-  @one_day_space = ''.center(3)
-  @begin_of_month_wday = first_day.wday
 end
 
-def begin_of_month_positioning
-  number_for_sunday = 0
-  number_for_saturday = 6
-  first_day = 1
-  if @begin_of_month_wday == number_for_sunday
-    print "#{first_day}".center(3)
-  elsif @begin_of_month_wday == number_for_saturday
-    (@begin_of_month_wday).times do
-      print @one_day_space
-    end
-    print "#{first_day}\n".rjust(3)
-  else
-    (@begin_of_month_wday).times do
-      print @one_day_space
-    end
-    print "#{first_day}".center(3)
+
+def begin_of_month_positioning(wday_begin_of_month)
+  one_day_space = "\s" * 3
+  (wday_begin_of_month).times do
+    print one_day_space
   end
 end
 
-def show_calendar(year: @today.year , month: @today.month)
+
+def show_calendar(year:, month:, wday_begin_of_month:)
   last_day = Date.new(year, month, -1).day
   week_days = 7
   row_count_number = 1
+  wday_for_saturday = 6
 
   calendar_head(year,month)
-
   1.upto(last_day) do |day|
     if day == 1
-      begin_of_month_positioning()
-    elsif day == (week_days - @begin_of_month_wday)
-      print "#{day}\n".rjust(3)
-      row_count_number = 1
-    elsif row_count_number == week_days
+      begin_of_month_positioning(wday_begin_of_month)
+      print "#{day}".center(3)
+      print "\n" if wday_begin_of_month == wday_for_saturday
+    elsif day == (week_days - wday_begin_of_month) || row_count_number == week_days
       print "#{day}\n".rjust(3)
       row_count_number = 1
     else
@@ -58,11 +43,15 @@ def show_calendar(year: @today.year , month: @today.month)
 end
 
 
-
-
 optional_value = ARGV.getopts('y:m:')
-optional_value.compact!
-optional_value = optional_value.map {|key,value| [key,value.to_i]}.to_h
+today = Date.today
+optional_value['y'] ||= today.year
+optional_value['m'] ||= today.month
+
+if optional_value['y'].is_a?(String) || optional_value['m'].is_a?(String)
+  optional_value = optional_value.map {|key,value| [key,value.to_i]}.to_h
+end
+wday_begin_of_month = Date.new(optional_value['y'], optional_value['m'], 1).wday
 
 unless optional_value['m'].nil?
   unless (1 .. 12).include?(optional_value['m'])
@@ -70,17 +59,9 @@ unless optional_value['m'].nil?
   end
 end
 
-if optional_value.empty?
-  show_calendar()
+if message
+  puts message
+  return
 else
-  if message
-    puts message
-    return
-  elsif optional_value.has_key?('y') && optional_value.has_key?('m')
-    show_calendar(year: optional_value['y'],month: optional_value['m'])
-  elsif optional_value.has_key?('y')
-    show_calendar(year: optional_value['y'])
-  elsif optional_value.has_key?('m')
-    show_calendar(month: optional_value['m'])
-  end
+  show_calendar(year: optional_value['y'], month: optional_value['m'], wday_begin_of_month: wday_begin_of_month)
 end
