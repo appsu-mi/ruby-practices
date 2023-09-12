a43906e47eff3e15d4b8c88a97f2d29821e7e4b0
appsu-mi <natubate870@gmail.com>
2023-09-13 07:37:59 +0900

    Calendar Refactoring Fourth

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -7,21 +7,20 @@ ONE_DAY_SPACE = " " * 3
 
 def calendar_head(year, month)
   week_a_words = ['日', '月', '火', '水', '木', '金', '土']
-  puts "#{month}月 #{year}".rjust(13)
-  week_a_words.each {|word| print "#{word}".ljust(2)}
+  puts "#{ONE_DAY_SPACE * 2}#{month}月 #{year}"
+  week_a_words.each {|word| print "#{word} "}
   puts "\n"
 end
 
 def show_calendar(year:, month:, wday_begin_of_month:)
+  first_week_of_the_month = WEEK_DAY - wday_begin_of_month
   last_day = Date.new(year, month, -1).day
   calendar_head(year,month)
 
   1.upto(last_day) do |day|
     print ONE_DAY_SPACE * wday_begin_of_month if day == 1
-    day < 10 ? print("#{day}".center(3)) : print("#{day}".ljust(3))
-    if day == (WEEK_DAY - wday_begin_of_month)
-      print "\n"
-    elsif (day - (WEEK_DAY - wday_begin_of_month)) % WEEK_DAY == 0
+    day > 9 ? print("#{day} ") : print(" #{day} ")
+    if day == first_week_of_the_month || (day - first_week_of_the_month) % WEEK_DAY == 0
       print "\n"
     end
   end
