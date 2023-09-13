9ccda677d88ece9e4351dbe7aaf7dfe3a882a283
appsu-mi <natubate870@gmail.com>
2023-09-13 21:43:34 +0900

    Date display refactoring

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -7,7 +7,7 @@ ONE_DAY_SPACE = " " * 3
 
 def calendar_head(year, month)
   week_a_words = ['日', '月', '火', '水', '木', '金', '土']
-  puts "#{ONE_DAY_SPACE * 2}#{month}月 #{year}"
+  puts ' ' * 6 + "#{month}月 #{year}"
   week_a_words.each {|word| print "#{word} "}
   puts "\n"
 end
@@ -16,11 +16,11 @@ def show_calendar(year:, month:, wday_begin_of_month:)
   first_week_of_the_month = WEEK_DAY - wday_begin_of_month
   last_day = Date.new(year, month, -1).day
   calendar_head(year,month)
-
-  1.upto(last_day) do |day|
-    print ONE_DAY_SPACE * wday_begin_of_month if day == 1
-    day > 9 ? print("#{day} ") : print(" #{day} ")
-    if day == first_week_of_the_month || (day - first_week_of_the_month) % WEEK_DAY == 0
+  1.upto(last_day) do |day_count|
+    day = Date.new(year, month, day_count)
+    print ONE_DAY_SPACE * wday_begin_of_month if day_count == 1
+    print day.strftime('%e') + ' '
+    if day_count == first_week_of_the_month || (day_count - first_week_of_the_month) % WEEK_DAY == 0
       print "\n"
     end
   end
@@ -33,3 +33,5 @@ optional_value['m'] ||= today.month
 wday_begin_of_month = Date.new(optional_value['y'], optional_value['m'], 1).wday
 
 show_calendar(year: optional_value['y'], month: optional_value['m'], wday_begin_of_month: wday_begin_of_month)
+
+
