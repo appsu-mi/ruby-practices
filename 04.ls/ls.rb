79b78677c77a63a1ccbcd28335794b0ee48d5ccc
appsu-mi <natubate870@gmail.com>
2023-10-06 10:41:08 +0900

    Modified variables, etc.

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,26 +1,28 @@
 # frozen_string_literal: true
 
-COLUMN_NUMBER = 3
+COLUMN_SIZE = 3
 
 def main
-  directory_files = Dir.children(ARGV[0] || '.').reject { |file| file[0] == '.' }.sort
-  max_length = directory_files.map(&:length).max
-  show_ls(directory_files, max_length)
+  files = Dir.children(ARGV[0] || '.').reject { |file| file[0] == '.' }.sort
+  max_length = files.map(&:length).max
+  show_ls(files, max_length)
 end
 
-def show_ls(directory_files, max_length)
-  split_files(directory_files).transpose.each do |row_files|
-    row_files.each { |col_file| col_file == row_files.last ? print(col_file) : printf("%-#{max_length}s\t", col_file) }
+def show_ls(files, max_length)
+  split_files(files).transpose.each do |row_files|
+    row_files.each.with_index(1) do |col_file, index|
+      index == row_files.length ? print(col_file) : printf("%-#{max_length}s\t", col_file)
+    end
     puts
   end
 end
 
-def split_files(directory_files)
-  used_slice_number = (directory_files.length / COLUMN_NUMBER.to_f).ceil
-  division_files = directory_files.each_slice(used_slice_number).to_a
-  (used_slice_number - division_files[-1].length).times { division_files[-1] << nil }
+def split_files(files)
+  used_slice_size = (files.length / COLUMN_SIZE.to_f).ceil
+  divided_files = files.each_slice(used_slice_size).to_a
+  (used_slice_size - divided_files[-1].length).times { divided_files[-1] << nil }
 
-  division_files
+  divided_files
 end
 
 main
