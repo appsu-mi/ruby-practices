09f68d8e8d7373001b61990407aa04e02636eac8
appsu-mi <natubate870@gmail.com>
2023-10-24 18:20:36 +0900

    Modified: First refactoring.

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -10,16 +10,16 @@ def main
   pattern = '*'
   path = ARGV[0] || '.'
   file_names = Dir.glob(pattern, base: path)
-  max_length_name = file_names.map(&:length).max
-  l_option ? load_l_option(path, file_names) : show_ls(file_names, max_length_name)
+  l_option ? load_l_option(path, file_names) : load_ls(file_names)
 end
 
-def show_ls(files, max_length_name)
+def load_ls(files)
+  max_length = files.map(&:length).max
   split_files(files).transpose.each do |row_files|
     row_files.each.with_index(1) do |col_file, index|
       next if col_file.nil?
 
-      index == row_files.length ? print(col_file) : printf("%-#{max_length_name}s\t", col_file)
+      index == row_files.length ? print(col_file) : printf("%-#{max_length}s\t", col_file)
     end
     puts
   end
@@ -33,15 +33,7 @@ def split_files(files)
   divided_files
 end
 
-PERMISSION = {
-  '0' => '---',
-  '1' => '--x',
-  '2' => '-w-',
-  '3' => '-wx',
-  '4' => 'r--',
-  '5' => 'r-x',
-  '6' => 'rw-',
-  '7' => 'rwx',
+FILE_TYPE = {
   'fifo' => 'p',
   'characterSpecial' => 'c',
   'directory' => 'd',
@@ -51,13 +43,24 @@ PERMISSION = {
   'socket' => 's'
 }.freeze
 
-def load_l_option(path, file_names)
-  stat_files = file_names.map { |faile_name| File.lstat("#{path}/#{faile_name}") }
-  path_to_stat = stat_files.map.with_index { |stat, idx| ["#{path}/#{file_names.fetch(idx)}", stat] }.to_h
+PERMISSION = {
+  '0' => '---',
+  '1' => '--x',
+  '2' => '-w-',
+  '3' => '-wx',
+  '4' => 'r--',
+  '5' => 'r-x',
+  '6' => 'rw-',
+  '7' => 'rwx'
+}.freeze
 
-  puts "total #{stat_files.map(&:blocks).sum}"
-  load_body(path_to_stat).each do |file|
-    puts format_file(file, stat_files)
+def load_l_option(path, file_names)
+  stat_files = file_names.map { |file_name| File.lstat("#{path}/#{file_name}") }
+  path_to_stat = stat_files.map.with_index { |stat, idx| ["#{path}/#{file_names[idx]}", stat] }.to_h
+
+  puts "total #{stat_files.sum(&:blocks)}"
+  load_body(path_to_stat).each do |file_show_data|
+    puts format_file(file_show_data, stat_files)
   end
 end
 
@@ -69,77 +72,79 @@ def load_body(path_to_stat)
       owner: Etc.getpwuid(stat.uid).name,
       group: Etc.getgrgid(stat.gid).name,
       byte_size: stat.size.to_s,
-      time_stamp: stat.mtime.strftime(
-        Time.now.year == stat.mtime.year ? ' %_m %_d %H:%M ' : ' %_m %_d  %Y '
-      ),
-      file_name:
-        if stat.symlink?
-          "#{File.basename(absolute_path)} -> #{File.readlink(absolute_path)}"
-        else
-          File.basename(absolute_path)
-        end
+      time_stamp: stat.mtime.strftime(to_timestamp(stat.mtime.year)),
+      file_name: to_file_name(stat, absolute_path)
     }
   end
 end
 
-def format_file(file, stat_files)
-  block_to_maxlength = fetch_max_length(stat_files)
-  constant_size = 2
+def to_timestamp(year)
+  Time.now.year == year ? ' %_m %_d %H:%M ' : ' %_m %_d  %Y '
+end
+
+def to_file_name(stat, absolute_path)
+  file_name = File.basename(absolute_path)
+  stat.symlink? ? "#{file_name} -> #{File.readlink(absolute_path)}" : file_name
+end
+
+def format_file(file_show_data, stat_files)
+  section_to_maxlength = fetch_maxlength(stat_files)
+  space = ' '
   [
-    file.fetch(:permission),
-    file.fetch(:link_count).rjust(block_to_maxlength.fetch(:link) + constant_size), ' ',
-    file.fetch(:owner).ljust(block_to_maxlength.fetch(:owner)     + constant_size),
-    file.fetch(:group).ljust(block_to_maxlength.fetch(:group)     + constant_size),
-    file.fetch(:byte_size).rjust(block_to_maxlength.fetch(:byte_size)),
-    file.fetch(:time_stamp),
-    file.fetch(:file_name)
+    file_show_data[:permission], space * 2,
+    file_show_data[:link_count].rjust(section_to_maxlength[:link]), space,
+    file_show_data[:owner].ljust(section_to_maxlength[:owner]), space * 2,
+    file_show_data[:group].ljust(section_to_maxlength[:group]), space * 2,
+    file_show_data[:byte_size].rjust(section_to_maxlength[:byte_size]),
+    file_show_data[:time_stamp],
+    file_show_data[:file_name]
   ].join
 end
 
-def fetch_max_length(stat_files)
-  block_to_all_value = { link: [], owner: [], group: [], byte_size: [] }
+def fetch_maxlength(stat_files)
+  section_to_length = { link: 0, owner: 0, group: 0, byte_size: 0 }
+
   stat_files.each do |stat|
-    block_to_all_value.fetch(:link)     << stat.nlink
-    block_to_all_value.fetch(:owner)    << Etc.getpwuid(stat.uid).name
-    block_to_all_value.fetch(:group)    << Etc.getgrgid(stat.gid).name
-    block_to_all_value.fetch(:byte_size) << stat.size
+    section_to_length[:link] = stat.nlink.to_s.length if section_to_length[:link] < stat.nlink.to_s.length
+    section_to_length[:owner] = Etc.getpwuid(stat.uid).name.length if section_to_length[:owner] < Etc.getpwuid(stat.uid).name.length
+    section_to_length[:group] = Etc.getgrgid(stat.gid).name.length if section_to_length[:group] < Etc.getgrgid(stat.gid).name.length
+    section_to_length[:byte_size] = stat.size.to_s.length if section_to_length[:byte_size] < stat.size.to_s.length
   end
-  block_to_all_value.transform_values { |values| values.map { |value| value.to_s.length }.max }
+  section_to_length
 end
 
 def to_permission(stat)
-  [
-    PERMISSION[stat.ftype], divide_character(stat).map do |block, char|
-      to_uid_or_gid(stat, block, char) || to_sticky(stat, block, char) || PERMISSION[char]
-    end
-  ].join
+  FILE_TYPE[stat.ftype] + divide_character(stat).map do |section, char|
+    to_uid_or_gid(stat, section, char) || to_sticky(stat, section, char) || PERMISSION[char]
+  end.join
 end
 
 def divide_character(stat)
   # mode.to_s(8)の戻り値の文字数は可変なため、末尾から指定しています。
   stat.mode.to_s(8).slice(-3..-1).chars.map.with_index do |char, idx|
     case idx
-    when 0 then [:user,  char]
+    when 0 then [:user, char]
     when 1 then [:group, char]
     when 2 then [:other, char]
     end
   end.to_h
 end
 
-def to_uid_or_gid(stat, block, char)
-  return unless block == :user && stat.setuid? || block == :group && stat.setgid?
+def to_uid_or_gid(stat, section, char)
+  return unless section == :user && stat.setuid? || section == :group && stat.setgid?
 
   PERMISSION[char].sub(/#{PERMISSION[char].slice(-1)}\z/, to_special_permission(:setuid_gid))
 end
 
-def to_sticky(stat, block, char)
-  return unless block == :other && stat.sticky?
+def to_sticky(stat, section, char)
+  return unless section == :other && stat.sticky?
 
   PERMISSION[char].sub(/#{PERMISSION[char].slice(-1)}\z/, to_special_permission(:sticky))
 end
 
-def to_special_permission(block)
-  { setuid_gid: { 'x' => 's', '-' => 'S' }, sticky: { 'x' => 't', '-' => 'T' } }.fetch(block)
+# これはセクションじゃない気がするね
+def to_special_permission(key)
+  { setuid_gid: { 'x' => 's', '-' => 'S' }, sticky: { 'x' => 't', '-' => 'T' } }[key]
 end
 
 main
